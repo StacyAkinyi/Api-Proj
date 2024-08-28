d5809bc89dee697cd333c8088dad42de6d2bdddb
StacyAkinyi <stacy.akinyi@gmail.com>
2024-08-28 15:02:07 +0300

    added class fnc

---
+++ fnc.php
@@ -0,0 +1,11 @@
+<?php
+class fnc{/*class: keyword that informs the program to create a class, fnc- function
+             Only one clas should be created*/
+    var $fname;
+    public $username;
+    var $yob;
+    protected $email_address;
+    private $password;
+
+   
+}
